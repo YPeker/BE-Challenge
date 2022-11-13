5d27f5b9b86c2057e5b1cfa78e57daf58c775b47
ypeker <ypeker@Ubuntu.myguest.virtualbox.org>
2022-11-13 18:32:08 +0100

    fix: profile check for GET /contracts/:id endpoint

--- src/app.ts
+++ src/app.ts
@@ -15,8 +15,7 @@ app.get('/contracts/:id',getProfile , async (req, res) =>{
     const {Contract} = req.app.get('models')
     const {id} = req.params
     const contract = await Contract.findOne({where: {id}})
-
-    if(req.profile !== id) return res.status(401).end()
+    if(req.profile.id !== contract.ContractorId && req.profile.id !== contract.ClientId) return res.status(401).end()
     if(!contract) return res.status(404).end()
     res.json(contract)
 })
